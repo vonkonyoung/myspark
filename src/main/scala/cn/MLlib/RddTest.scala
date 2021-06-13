c71e591373acd3a5c0b849b31a8a2cc310b5144a
fengjinrong <fengjr@chinatelecom.cn>
2021-06-13 10:32:33 +0800

    初始化

--- src/main/scala/cn/MLlib/RddTest.scala
+++ src/main/scala/cn/MLlib/RddTest.scala
@@ -4,13 +4,25 @@ object RddTest {
 
   def main(args: Array[String]): Unit = {
 
-    val data=Array(1,2,3,4,5,6,7,8,9)
+   
     val conf = new SparkConf()
       .setMaster("local[1]")
       .setAppName("WordCount");
     val sc = new SparkContext(conf)
+    val rdd1=sc.parallelize(1 to 9,3)
+    val rdd2=rdd1.map(x=>x*2)
+    println(rdd2.collect().mkString(","))
+    sc.stop()
+//    createRdd(sc)
+
+
+
+  }
+
+  def createRdd(sc:SparkContext): Unit ={
+    val data=Array(1,2,3,4,5,6,7,8,9)
     val distData=sc.parallelize(data,3)
     println(distData.take(3).mkString(","))
-
+    sc.stop()
   }
 }
